6245477fc3c90e8e9caa083268d5eb52a9c225b3
Yasnnie <dev.cyas@gmail.com>
2024-04-05 16:24:57 -0300

    feat: adicionado a documentação do swagger

--- src/app.js
+++ src/app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('./routes/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes/index");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 const port = 3001;
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', routes);
+app.use("/", routes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
